96c6849c1763c5547448ac43af8c5ac969737b64
PRO100 Cliry <128536194+Cliry1@users.noreply.github.com>
2024-05-19 02:46:53 +0300

    Update 1.ts

--- vite-project/src/generics/1.ts
+++ vite-project/src/generics/1.ts
@@ -2,7 +2,7 @@ import axios, {AxiosResponse}  from 'axios';
 
 async function fetchData <T>(url:string): Promise<T> {
   try {
-    const response: AxiosResponse = await axios.get(url);
+    const response: AxiosResponse<T> = await axios.get<T>(url);
     return response.data;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
